aa508de47af566a5d631ddf2eaea0973cbad0f1a
Mutiara A'yun <mutiara.ayun19@gmail.com>
2022-10-28 16:34:45 +0700

    feat(product-highlight): homepage implementation

--- components/ProductsComponent/index.tsx
+++ components/ProductsComponent/index.tsx
@@ -1,14 +1,16 @@
 /* library package */
 import { FC, useState } from 'react'
 /* component */
-import ProductsWidget from './ProductsWidget'
 import ProductsList from './ProductsList'
+import ProductsWidget from './ProductsWidget'
+import ProductHighlight from './ProductHighlight'
+import ProductsCategory from './ProductsCategory'
 import ProductRecomendation from './ProductRecomendation'
 /* styles */
 import styles from 'public/scss/components/Product.module.scss'
 
 export type ProductsComponentType = {
-  type: "list" | "widget" | "recomendation"
+  type: "list" | "widget" | "recomendation" | "category" | "highlight 1" | "highlight 2"
   i18n: any
   lng: string
   slug?: string | string[]
@@ -36,6 +38,20 @@ const classesProducts = {
   saleLabelClassName: styles.product_stickerItemSale,
   preOrderLabelClassName: styles.product_stickerItemPreOrder,
   newLabelClassName: styles.product_stickerItemNew,
+  /* Product Highlight */
+  productHighlightContainerClassName: styles.productHighlight_Container,
+  productHighlightTitleContainerClassName: styles.productHighlight_TitleContainer,
+  productHighlightTitleClassName: styles.productHighlight_Title,
+  productSectionContainerClassName: styles.productHighlight_productSectionContainer,
+  productHighlightSeeAllClassName: styles.productHighlight_SeeAll,
+  /* Product Category */
+  parentCategoryClassName: styles.category_productCategory,
+  categoryItemClassName: `${styles.category_items} col-6 col-md-3`,
+  categoryValueClassName: styles.category_itemsValue,
+  dropdownIconClassName: "d-none",
+  imgContainerClassName: styles.category_imageContainer,
+  imgClassName: styles.category_itemsImage,
+  categoryNameClassName: styles.category_itemsImagesName
 }
 
 const classesPlaceholderProducts = {
@@ -52,7 +68,7 @@ const ProductsComponent: FC<ProductsComponentType> = ({
   collectionSlug,
   filterProduct,
   getTotalProduct,
-  lng
+  lng,
 }) => {
   const [totalProducts, setTotalProducts] = useState(null)
 
@@ -86,7 +102,27 @@ const ProductsComponent: FC<ProductsComponentType> = ({
       classPlaceholder={classesPlaceholderProducts}
       setTotalProducts={setTotalProducts}
     />
-  ) : (<></>)
+  ) : type === "category" ? (
+    <ProductsCategory
+      i18n={i18n}
+      classProducts={classesProducts}
+      classPlaceholder={classesPlaceholderProducts}
+    />
+  ) : type === "highlight 1" ? (
+    <ProductHighlight
+      itemPerPage={itemPerPage}
+      classProducts={classesProducts}
+      classPlaceholder={classesPlaceholderProducts}
+      display={'Display1'}
+    />
+  ) :  type === "highlight 2" ? (
+    <ProductHighlight
+      itemPerPage={itemPerPage}
+      classProducts={classesProducts}
+      classPlaceholder={classesPlaceholderProducts}
+      display={'Display2'}
+    />
+  ) :(<></>)
 }
 
 export default ProductsComponent
