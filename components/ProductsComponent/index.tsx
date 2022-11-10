e135a364a865a32764197de06fc7aaa5baeb7186
Mutiara A'yun <mutiara.ayun19@gmail.com>
2022-11-10 16:31:58 +0700

    remove comments code

--- components/ProductsComponent/index.tsx
+++ components/ProductsComponent/index.tsx
@@ -2,7 +2,6 @@
 import { FC, useState } from 'react'
 /* component */
 import ProductsList from './ProductsList'
-// import ProductsWidget from './ProductsWidget'
 import ProductHighlight from './ProductHighlight'
 import ProductsCategory from './ProductsCategory'
 import ProductRecomendation from './ProductRecomendation'
@@ -65,7 +64,6 @@ const classesPlaceholderProducts = {
 const ProductsComponent: FC<ProductsComponentType> = ({
   type = "list",
   i18n,
-  // tagName,
   slug,
   SKUs,
   itemPerPage = 4,
