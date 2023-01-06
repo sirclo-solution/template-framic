c6ecc91f5d890e9b295cc98c4d1f78e07f2dec0d
Annisa Dian Nugrahani <78912388+nisadn@users.noreply.github.com>
2023-01-06 09:18:46 +0700

    feat(premium): hide register link (#69)
    * feat(premium): hide register link
    
    * chore: rename variable
    
    * chore: rename variable isTermOfPayment
    
    * chore: change nexus version

--- pages/[lng]/register/index.tsx
+++ pages/[lng]/register/index.tsx
@@ -16,7 +16,8 @@ import {
 import { 
   Register, 
   useI18n, 
-  useAuthToken
+  useAuthToken,
+  isTermOfPayment
 } from '@sirclo/nexus'
 /* library template */
 import redirectIfAuthenticated from 'lib/redirectIfAuthenticated'
@@ -69,6 +70,7 @@ const RegisterPage: FC<any> = ({
 
   const [isVerified, setIsVerified] = useState<boolean>(false)
   const linksBreadcrumb = [i18n.t("header.home"), i18n.t("register.title")]
+  const isTermOfPaymentAllowed = isTermOfPayment();
 
   const getReCAPTCHAToken = async () => {
     const token = await recaptchaRef.current.executeAsync()
@@ -100,6 +102,7 @@ const RegisterPage: FC<any> = ({
       lngDict={lngDict}
       brand={brand}
       setSEO={{ title: i18n.t("register.title") }}
+      withAllowed={!isTermOfPaymentAllowed}
     >
       <Breadcrumb links={linksBreadcrumb} lng={lng} />
       <div className={styles.register_wrapper}>
