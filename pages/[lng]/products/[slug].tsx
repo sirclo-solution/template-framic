d1fedd27e2855008939f9cd1a5002c05a7f0cd37
Virginia Ritonga <virginia.p.m@mail.ugm.ac.id>
2023-01-12 17:03:27 +0700

    fix: build conflict

--- pages/[lng]/products/[slug].tsx
+++ pages/[lng]/products/[slug].tsx
@@ -91,10 +91,14 @@ const ProductHighlightPage: FC<any> = ({
 
 export const getServerSideProps: GetServerSideProps = async ({
   req,
+  res,
   params,
 }) => {
   const { slug } = params
-  const brand = await useBrand(req)
+  const [brand] = await Promise.all([
+    useBrand(req),
+    useAuthToken({ req, res, env: process.env })
+  ])
   const defaultLanguage = brand?.settings?.defaultLanguage || params.lng || 'id'
   const { default: lngDict = {} } = await import(`locales/${defaultLanguage}.json`)
 
