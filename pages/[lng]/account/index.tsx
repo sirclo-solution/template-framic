cc528fe2fcc6fc9990dc6014be099c6b126b6ffc
Anggara Diebrata <angga.deebrata@gmail.com>
2023-04-18 13:31:25 +0800

    add changes in index account

--- pages/[lng]/account/index.tsx
+++ pages/[lng]/account/index.tsx
@@ -227,7 +227,7 @@ const AccountsPage: FC<AccountPageProps> = ({
   const onSuccessChPass = (msg: string) => toast.success(msg)
 
   const onSuccess = (msg: string, data: any) => {
-    setName(data?.upsertProfile[0]?.firstName + " " + data?.upsertProfile[0]?.lastName)
+    if (data && data?.upsertProfile) setName(data?.upsertProfile[0]?.firstName + " " + data?.upsertProfile[0]?.lastName)
     toast.success(msg)
   }
 
